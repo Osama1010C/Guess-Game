4beec2fe46f18b611d9259d2c53c12a152d909e7
OSaMa <166417491+Osama1010C@users.noreply.github.com>
2024-07-20 15:36:50 +0300

    Add files via upload

---
+++ Guess Game/Guess Game/Hint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Game
+{
+    class Hint
+    {
+        private int number;
+        private static List<string> hintsList;
+
+        private int Number => this.number;
+
+        public Hint(int number)
+        {
+            this.number = number;
+            hintsList = new List<string>();
+        }
+        public Hint() : this(0)
+        {
+
+        }
+
+        public void CreateHints()
+        {
+            if (Number % 2 == 0) hintsList.Add("The Number Is Even");
+            if (Number % 2 != 0) hintsList.Add("The Number Is Odd");
+            if (Number % 4 == 0) hintsList.Add("The Number Is Divisible by 4");
+            if (Number % 5 == 0) hintsList.Add("The Number Is Divisible by 5");
+            int range1 = Number - 10 < 0 ? 0 : Number - 10;
+            int range2 = Number + 10;
+            hintsList.Add($"The Number Is Between {range1} And {range2}");
+            if (Number > 10) { hintsList.Add("Number Is Greater Than 10"); hintsList.Add("Number consist of 2 digits"); }
+            if (Number < 10) { hintsList.Add("Number Is Less Than 10"); hintsList.Add("Number consist of 1 digit"); }
+            if (Number > 25) hintsList.Add("Number Is Greater Than 25");
+            if (Number < 25) hintsList.Add("Number Is Less Than 25");
+            if (Number == 7) hintsList.Add("The Best Player In The World");
+            if (Number == 10) hintsList.Add("The Second Best Player In The World");
+            if (Number > 30) hintsList.Add("Number Is Greater Than 30");
+            if (Number < 30) hintsList.Add("Number Is Less Than 30");
+        }
+
+        public string GetHint()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, hintsList.Count());
+            string theHint = hintsList[randomNumber];
+            hintsList.RemoveAt(randomNumber);
+            return theHint;
+        }
+
+        private int NumberOfHints() => hintsList.Count();
+    }
+}
---
+++ Guess Game/Guess Game/Program.cs
@@ -0,0 +1,384 @@
+﻿using System.Diagnostics;
+
+namespace Guess_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            
+        }
+
+        static void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("======================\n      Guess Game\n======================\n");
+                Console.WriteLine("1] Start\n\n2] Info\n\n3] Exit\n");
+                int select1;
+                while (true)
+                {
+                    try
+                    {
+                        select1 = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unknown Choice");
+                        continue;
+                    }
+                    if (select1 != 1 && select1 != 2 && select1 != 3)
+                    {
+                        Console.WriteLine("Unknown Choice");
+                        continue;
+                    }
+                    else break;
+                }
+                
+                
+                if(select1 == 1)
+                {
+                    Console.Clear();
+                    while (true)
+                    {
+
+                        Console.WriteLine("======================\n      Guess Game\n======================\n");
+                        Console.WriteLine("1] Easy Level\n\n2] Normal Level\n\n3] Hard Level\n\n4] Back\n");
+                        int select2;
+                        while (true)
+                        {
+                            try
+                            {
+                                select2 = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Unknown Choice");
+                                continue;
+                            }
+                            if (select2 != 1 && select2 != 2 && select2 != 3 && select2 != 4)
+                            {
+                                Console.WriteLine("Unknown Choice");
+                                continue;
+                            }
+                            else break;
+                        }
+                        if (select2 == 1)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("======================\n      Guess Game\n======================\n");
+                            Random random = new Random();
+                            int rightNumber = random.Next(0,11);
+                            Hint hint = new Hint(rightNumber);
+                            hint.CreateHints();
+                            int availableHints = 3;
+                            int chances = 3;
+                            while (true)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("======================\n      Guess Game\n======================\n");
+                                Console.WriteLine($"Chances Left : {chances}  |  Hints : {availableHints}\n-------------------------------\n");
+                                Console.Write("Guess < -1 For a Hint > : ");
+                                if (chances == 0)
+                                {
+                                    Console.WriteLine($"You Lose The Answer Was {rightNumber}\n\n\n");
+                                    Console.WriteLine("< Click Enter To Back >");
+                                    string enter = Console.ReadLine();
+                                    break;
+                                }
+                                int guess;
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        guess = Convert.ToInt32(Console.ReadLine());
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine("Invalid Guess");
+                                        continue;
+                                    }
+                                    if (guess < 0 && guess != -1)
+                                    {
+                                        Console.WriteLine("Bro We Are Start From < Zero >");
+                                        continue;
+                                    }
+                                    else if (guess > 10)
+                                    {
+                                        Console.WriteLine($"Bro We Are End In < {10} >");
+                                        continue;
+                                    }
+                                    else break;
+                                }
+
+                                if (guess == -1)
+                                {
+                                    if (availableHints != 0)
+                                    {
+                                        Console.WriteLine($"Hint > {hint.GetHint()}");
+                                        availableHints--;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("You Don't Have Any Hints Left");
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (guess == rightNumber)
+                                    {
+                                        Console.WriteLine($"{guess} Is The Right Answer \n\nYou Win\n\n\n");
+                                        Console.WriteLine("< Click Enter To Back >");
+                                        string enter = Console.ReadLine();
+                                        break;
+
+                                    }
+                                    else
+                                    {
+                                        chances--;
+                                        Console.WriteLine("Wrong Answer\n");
+                                        if (chances == 0)
+                                        {
+                                            Console.WriteLine($"You Lose The Answer Was {rightNumber}\n\n\n");
+                                            Console.WriteLine("< Click Enter To Back >");
+                                            string enter = Console.ReadLine();
+                                            break;
+                                        }
+                                    }
+                                }
+                                Thread.Sleep(2000);
+                                Console.Clear();
+                            }
+
+                            Console.Clear();
+                        }
+                        else if (select2 == 2)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("======================\n      Guess Game\n======================\n");
+                            Random random = new Random();
+                            int rightNumber = random.Next(0, 26);
+                            Hint hint = new Hint(rightNumber);
+                            hint.CreateHints();
+                            int availableHints = 3;
+                            int chances = 5;
+                            while (true)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("======================\n      Guess Game\n======================\n");
+                                Console.WriteLine($"Chances Left : {chances}  |  Hints : {availableHints}\n-------------------------------\n");
+                                Console.Write("Guess < -1 For a Hint > : ");
+                                if (chances == 0)
+                                {
+                                    Console.WriteLine($"You Lose The Answer Was {rightNumber}\n\n\n");
+                                    Console.WriteLine("< Click Enter To Back >");
+                                    string enter = Console.ReadLine();
+                                    break;
+                                }
+                                int guess;
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        guess = Convert.ToInt32(Console.ReadLine());
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine("Invalid Guess");
+                                        continue;
+                                    }
+                                    if (guess < 0 && guess != -1)
+                                    {
+                                        Console.WriteLine("Bro We Are Start From < Zero >");
+                                        continue;
+                                    }
+                                    else if (guess > 25)
+                                    {
+                                        Console.WriteLine($"Bro We Are End In < {25} >");
+                                        continue;
+                                    }
+                                    else break;
+                                }
+
+                                if (guess == -1)
+                                {
+                                    if (availableHints != 0)
+                                    {
+                                        Console.WriteLine($"Hint > {hint.GetHint()}");
+                                        availableHints--;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("You Don't Have Any Hints Left");
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (guess == rightNumber)
+                                    {
+                                        Console.WriteLine($"{guess} Is The Right Answer \n\nYou Win\n\n\n");
+                                        Console.WriteLine("< Click Enter To Back >");
+                                        string enter = Console.ReadLine();
+                                        break;
+
+                                    }
+                                    else
+                                    {
+                                        chances--;
+                                        Console.WriteLine("Wrong Answer\n");
+                                        if (chances == 0)
+                                        {
+                                            Console.WriteLine($"You Lose The Answer Was {rightNumber}\n\n\n");
+                                            Console.WriteLine("< Click Enter To Back >");
+                                            string enter = Console.ReadLine();
+                                            break;
+                                        }
+                                    }
+                                }
+                                Thread.Sleep(2000);
+                                Console.Clear();
+                            }
+
+                            Console.Clear();
+                        }
+                        else if (select2 == 3)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("======================\n      Guess Game\n======================\n");
+                            Random random = new Random();
+                            int rightNumber = random.Next(0, 51);
+                            Hint hint = new Hint(rightNumber);
+                            hint.CreateHints();
+                            int availableHints = 4;
+                            int chances = 8;
+                            while (true)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("======================\n      Guess Game\n======================\n");
+                                Console.WriteLine($"Chances Left : {chances}  |  Hints : {availableHints}\n-------------------------------\n");
+                                Console.Write("Guess < -1 For a Hint > : ");
+                                if (chances == 0)
+                                {
+                                    Console.WriteLine($"You Lose The Answer Was {rightNumber}\n\n\n");
+                                    Console.WriteLine("< Click Enter To Back >");
+                                    string enter = Console.ReadLine();
+                                    break;
+                                }
+                                int guess;
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        guess = Convert.ToInt32(Console.ReadLine());
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine("Invalid Guess");
+                                        continue;
+                                    }
+                                    if (guess < 0 && guess != -1)
+                                    {
+                                        Console.WriteLine("Bro We Are Start From < Zero >");
+                                        continue;
+                                    }
+                                    else if (guess > 50)
+                                    {
+                                        Console.WriteLine($"Bro We Are End In < {50} >");
+                                        continue;
+                                    }
+                                    else break;
+                                }
+
+                                if (guess == -1)
+                                {
+                                    if (availableHints != 0)
+                                    {
+                                        Console.WriteLine($"Hint > {hint.GetHint()}");
+                                        availableHints--;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("You Don't Have Any Hints Left");
+                                    }
+
+                                }
+                                else
+                                {
+                                    if (guess == rightNumber)
+                                    {
+                                        Console.WriteLine($"{guess} Is The Right Answer \n\nYou Win\n\n\n");
+                                        Console.WriteLine("< Click Enter To Back >");
+                                        string enter = Console.ReadLine();
+                                        break;
+
+                                    }
+                                    else
+                                    {
+                                        chances--;
+                                        Console.WriteLine("Wrong Answer\n");
+                                        if (chances == 0)
+                                        {
+                                            Console.WriteLine($"You Lose The Answer Was {rightNumber}\n\n\n");
+                                            Console.WriteLine("< Click Enter To Back >");
+                                            string enter = Console.ReadLine();
+                                            break;
+                                        }
+                                    }
+                                }
+                                Thread.Sleep(2500);
+                                Console.Clear();
+                            }
+
+                            Console.Clear();
+                        }
+                        else
+                            break;
+                    }
+
+
+
+
+
+
+                    Console.Clear();
+                }
+                //select == 2
+                else if(select1 == 2)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("======================\n      Guess Game\n======================\n");
+                    Console.WriteLine("About :\n--------\n");
+                    Console.WriteLine("This Game Is About Guessing a Number And You Have Chances And Hints To Win The Game\n\n");
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                    Console.WriteLine("Levels :\n--------\n");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("1) Easy Level : ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write("Number Could be Between 0 And 10\n*] You Will Have ( 3 ) Chances And ( 3 ) Hints\n\n");
+                    Console.ForegroundColor= ConsoleColor.Blue;
+                    Console.Write("2) Normal Level : ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write("Number Could be Between 0 And 25\n*] You Will Have ( 5 ) Chances And ( 3 ) Hints\n\n");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("3) Hard Level : ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write("Number Could be Between 0 And 50\n*] You Will Have ( 8 ) Chances And ( 4 ) Hints\n\n");
+                    Console.WriteLine("==========================================================================================================\n\n");
+
+                    Console.WriteLine("< Click Enter To Back >");
+                    string enter = Console.ReadLine();
+                    
+                    Console.Clear();
+                }
+                else
+                {
+                    break;
+                }   
+            }            
+        }
+    }
+}
